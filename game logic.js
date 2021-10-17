d7a808e7d75e04efe11d37d2edc75a11f1fc2ba8
savKor <savvatejkorovin@gmail.com>
2021-10-17 20:43:47 +0300

    New files for work. Add html, js, css file.

---
+++ game logic.js
@@ -0,0 +1,52 @@
+const board = document.getElementById('board')
+let matrixSize=6;
+let winningCombination=3;
+
+
+function createMatrix (){
+  let matrix = [];
+  let n = matrixSize, m = matrixSize;
+  for(let i=0; i<n; i++) {
+      matrix[i] = getMatrixRowWithEmptyElement(m);
+  }
+  return matrix
+}
+
+function getMatrixRowWithEmptyElement(size) {
+  let row = []
+  for (let j=0; j<size; j++) {
+    row[j]=undefined;
+  }
+  return row;
+}
+
+let matrix = createMatrix();
+
+function renderMatrix() {
+  for (let i = 0; i < matrix.length; i++) {
+    const row = matrix[i];
+    renderRow(row, i)
+  }
+}
+
+
+function renderRow (row, rowIndex) {
+  let newDiv = document.createElement("div");
+  newDiv.className = 'row';
+  newDiv.id = 'row'+rowIndex;
+  board.appendChild(newDiv);
+  const rowElement = document.getElementById('row'+rowIndex)
+  rowElement.style.display = "flex";
+  for (let j = 0; j < row.length; j++) {
+    renderCell(rowIndex,j,rowElement)
+  }
+}
+
+function renderCell (rowIndex,columnIndex,rowElement) {
+  let newDiv = document.createElement("div");
+  newDiv.className = 'cell';
+  newDiv.id = rowIndex+"_"+columnIndex;
+  rowElement.appendChild(newDiv);
+}
+
+renderMatrix()
